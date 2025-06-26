4f9feca1ed5533a0a3efd79f9dc285f3fcaf9d1b
Shreya5780 <shreyachauhan1226@gmail.com>
2025-06-26 15:26:05 +0530

    Each time new session id

--- src/main/java/com/example/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/security/springsecurity/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.security.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,10 +18,19 @@ public class SecurityConfig {
 //        disable csrf(close)
         http.csrf(c -> c.disable());
 
-//        enale autherization
+//        desable autherization
         // because we write http.build with @bean it disable the autherization, so we need to enalee
 //        ?it show output as access to locahost is denied
         http.authorizeHttpRequests(r -> r.anyRequest().authenticated());
+
+//        enable login page
+//        http.formLogin(Customizer.withDefaults());
+
+//        to work with postman or rest api we need to do this
+        http.httpBasic(Customizer.withDefaults());
+
+//        to have each time different session id
+        http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
